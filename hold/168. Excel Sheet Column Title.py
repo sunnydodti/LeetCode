dc582b60e236e507b2f20292aed09460f832e63f
sunnydodti <sunnydodti@gmail.com>
2023-12-01 17:35:19 +0530

    2023-12-01 - 168 hold

---
+++ hold/168. Excel Sheet Column Title.py
@@ -0,0 +1,55 @@
+import string
+from typing import Dict, Union
+
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> Union[str, None]:
+
+        map: Dict[int, str] = {}
+        ans = columnNumber % 26
+        res = ""
+
+        for i, letter in enumerate(string.ascii_uppercase):
+            map[i+1] = letter
+        count = 0
+
+        def get_base26_number(num: int):
+            return map.get(num)
+
+        while columnNumber > 0:
+            columnNumber = columnNumber // 26
+            print("column", columnNumber)
+            count += 1
+            if columnNumber < 26:
+            if columnNumber >= 26:
+                res = get_base26_number(columnNumber) + res
+            print(res)
+        print("count", count)
+        return res
+
+    def convertToTitle(self, columnNumber: int) -> Union[str, None]:
+
+        map: Dict[int, str] = {}
+
+        for i, letter in enumerate(string.ascii_uppercase):
+            map[i+1] = letter
+        count = 0
+        ans = [""]
+        index = 1
+        num = 0
+        while columnNumber > 0:
+            columnNumber -= 26
+            if columnNumber > 0:
+                count += 1
+            if count <= 26:
+                ans[index] = map.get(count)
+            if count > 26:
+
+        return res
+
+
+if __name__ == "__main__":
+    S = Solution()
+    inp = 7+26
+    inp = 701
+    print(S.convertToTitle(inp))
